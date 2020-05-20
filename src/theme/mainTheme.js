324255158babeb468ebcf1b0a050f1a62f713c66
Jacek Grochowina <jacek99@vp.pl>
2020-05-20 17:55:10 +0200

    main theme - variables

--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -1,3 +1,26 @@
 export const theme = {
-
+    fontFamily: {
+        leading: "'Lato', sans-serif",
+        secondary: "'PT Sans', sans-serif",
+    },
+    fontWeight: {
+        regular: 400,
+        bold: 700,
+    },
+    fontSize: {
+        xxs: '1rem',
+        xs: '1.2rem',
+        s: '1.6rem',
+        m: '2.1rem',
+        l: '2.4rem',
+        xl: '4rem',
+    },
+    color: {
+        leading: "#9B556E",
+        dark: "#262626",
+        gray: "#707070",
+        light: "#D4D4D4",
+        superLight: "#ECEDED",
+        white: "#FFFFFF", 
+    }
 };
