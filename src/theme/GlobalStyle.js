8cd165cd6e78dd69bb2fdc781ec100703eebe3aa
Jacek Grochowina <jacek99@vp.pl>
2020-05-27 12:38:28 +0200

    fix styles on deployment

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,15 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-    /*
-     * import fonts
-     *  Lato:     regular 400
-     *            bold 700
-     *  PT Sans:  regular 400
-     *            bold 700
-     */
-    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=PT+Sans:wght@400;700&display=swap');
-
     *, *::before, *::after {
         margin: 0;
         padding: 0;
