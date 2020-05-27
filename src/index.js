ea985989b9e100f757253178c9eed59297accfb8
Jacek Grochowina <jacek99@vp.pl>
2020-05-27 13:00:12 +0200

    deployment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import { HashRouter } from 'react-router-dom'
 import Root from './views/Root';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <HashRouter>
+      <Root />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
