884ccb449bc70caa7631373a2df383282bbc5f36
Jacek Grochowina <jacek99@vp.pl>
2020-05-27 10:25:56 +0200

    add routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,10 @@
-export const routes = {
+const routes = {
     home: '/',
+    webdesign: '/tworzenie-stron-internetowych',
+    takecare: '/opieka-nad-stronami-internetowymi',
+    design: '/projektowanie-grafiki',
+    contact: '/contact',
+    rodo: '/polityka-prywatnosci',
 };
+
+export default routes;
