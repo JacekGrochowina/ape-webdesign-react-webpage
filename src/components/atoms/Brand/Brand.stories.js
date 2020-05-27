b2c78b71b250b82f4575766fdc9e05b288cd80d6
Jacek Grochowina <jacek99@vp.pl>
2020-05-27 10:22:48 +0200

    add stories

--- src/components/atoms/Brand/Brand.stories.js
+++ src/components/atoms/Brand/Brand.stories.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import StoryRouter from 'storybook-react-router';
 import { storiesOf } from '@storybook/react';
 import Brand from './Brand';
 
 storiesOf('Atoms/Brand', module)
+    .addDecorator(StoryRouter())
     .add('Logo', () => <Brand logo/>)
     .add('Logotype', () => <Brand logotype/>)
     .add('Custom width', () => <Brand logotype width={300} />)
